722ec0be69823aa9d5dd580d23903a60e315dda7
Santhoshi Vijaykumar Nanikeri <santhoshi.nanikeri@gmail.com>
2023-04-05 12:00:27 +0530

    Commiting FoodApp files

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React! My first React program."
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
